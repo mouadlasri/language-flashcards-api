47a51e49de30a4dae4ac0924f25c2c547a2b8bad
Mouad Lasri <mouadkor20@gmail.com>
2026-07-23 16:06:50 +0900

    first commit

---
+++ src/main/java/org/practice/languageflashcardsapi/LanguageFlashcardsApiApplication.java
@@ -0,0 +1,13 @@
+package org.practice.languageflashcardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageFlashcardsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageFlashcardsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/practice/languageflashcardsapi/LanguageFlashcardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.practice.languageflashcardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageFlashcardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
